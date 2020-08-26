d5d6a22eb9b85a4982bac1ae1430146863118353
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-08-26 12:50:23 +1200

    Playing with badges

---
+++ w_test_badges.py
@@ -0,0 +1,8 @@
+from cli_badges import badge
+
+successBadge = badge('success','8', messagebg='green',messagecolor='black')
+failedBadge = badge("failed",'1',messagebg='red')
+skippedBadge = badge('skipped', '1', messagebg='yellow',messagecolor='black')
+
+
+print(successBadge, failedBadge, skippedBadge, "")
