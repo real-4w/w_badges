db391185017984007022d649fb59eb558b8f67c0
Willem van der Steen <vandersw@pgtrust.co.nz>
2020-08-26 12:50:53 +1200

    Playing with badges

--- w_test_badges.py
+++ w_test_badges.py
@@ -4,5 +4,4 @@ successBadge = badge('success','8', messagebg='green',messagecolor='black')
 failedBadge = badge("failed",'1',messagebg='red')
 skippedBadge = badge('skipped', '1', messagebg='yellow',messagecolor='black')
 
-
 print(successBadge, failedBadge, skippedBadge, "")
